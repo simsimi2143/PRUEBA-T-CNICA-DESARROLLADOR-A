13d5989355fc4df5f8d7b3696dfd9a6390c26605
simsimi2143 <josuerivasvergara@gmail.com>
2026-04-19 23:36:32 -0400

    definicion de rutas

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SolicitudController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('solicitudes.index');
 });
+
+Route::resource('solicitudes', SolicitudController::class);
